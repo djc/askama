760752e79da94511862cb1cc4d73e881d77ccbf9
Dirkjan Ochtman <dirkjan@ochtman.nl>
2016-12-24 10:48:43 +0100

    Return AST nodes from parser

--- askama_codegen/src/parser.rs
+++ askama_codegen/src/parser.rs
@@ -1,28 +1,35 @@
 use nom::{self, IResult};
 
-fn take_content(i: &[u8]) -> IResult<&[u8], &[u8]> {
+pub enum Node<'a> {
+    Lit(&'a [u8]),
+    Expr(&'a [u8]),
+}
+
+fn take_content(i: &[u8]) -> IResult<&[u8], Node> {
     if i.len() < 1 || i[0] == b'{' {
         return IResult::Error(error_position!(nom::ErrorKind::TakeUntil, i));
     }
     for (j, c) in i.iter().enumerate() {
         if *c == b'{' {
             if i.len() < j + 2 {
-                return IResult::Done(&i[..0], &i[..]);
+                return IResult::Done(&i[..0], Node::Lit(&i[..]));
             } else if i[j + 1] == '{' as u8 {
-                return IResult::Done(&i[j..], &i[..j]);
+                return IResult::Done(&i[j..], Node::Lit(&i[..j]));
             } else if i[j + 1] == '%' as u8 {
-                return IResult::Done(&i[j..], &i[..j]);
+                return IResult::Done(&i[j..], Node::Lit(&i[..j]));
             }
         }
     }
-    IResult::Done(&i[..0], &i[..])
+    IResult::Done(&i[..0], Node::Lit(&i[..]))
 }
 
-named!(var_expr, delimited!(tag!("{{"), take_until!("}}"), tag!("}}")));
+named!(expr_str, delimited!(tag!("{{"), take_until!("}}"), tag!("}}")));
 
-named!(parse_template< Vec<&[u8]> >, many1!(alt!(take_content | var_expr)));
+named!(expr_node<Node>, map!(expr_str, Node::Expr));
 
-pub fn parse<'a>(src: &'a str) -> Vec<&'a [u8]> {
+named!(parse_template< Vec<Node> >, many1!(alt!(take_content | expr_node)));
+
+pub fn parse<'a>(src: &'a str) -> Vec<Node> {
     match parse_template(src.as_bytes()) {
         IResult::Done(_, res) => res,
         _ => panic!("problems parsing template source"),
