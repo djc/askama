2000af89016f6145bf42b72afaafe68c0a8da4a4
Dirkjan Ochtman <dirkjan@ochtman.nl>
2017-01-06 11:23:21 +0100

    Add basic test case for filters

---
+++ testing/tests/filters.rs
@@ -0,0 +1,22 @@
+#![feature(proc_macro)]
+
+#[macro_use]
+extern crate askama_derive;
+extern crate askama;
+
+use askama::Template;
+
+#[derive(Template)]
+#[template(path = "filters.html")]
+struct TestTemplate {
+    strvar: String,
+}
+
+#[test]
+fn filter_escape() {
+    let s = TestTemplate {
+        strvar: "my <html> is unsafe & should be escaped".to_string(),
+    };
+    assert_eq!(s.render(),
+               "my &lt;html&gt; is unsafe &amp; should be escaped\n");
+}
