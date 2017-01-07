9837efad983dd0de6439a8c9d59165df01adfe3f
Dirkjan Ochtman <dirkjan@ochtman.nl>
2017-01-07 20:24:06 +0100

    Get rid of obsolete test code and unused dependency

--- askama/src/lib.rs
+++ askama/src/lib.rs
@@ -1,28 +1,7 @@
 #![feature(proc_macro)]
 
-#[macro_use]
-extern crate askama_derive;
-
 pub trait Template {
     fn render(&self) -> String;
 }
 
 pub mod filters;
-
-#[cfg(test)]
-mod tests {
-
-    extern crate askama;
-
-    #[derive(Template)]
-    struct TestTemplate {
-        var: String,
-    }
-
-    #[test]
-    fn it_works() {
-        let s = TestTemplate { var: "foo".to_string() }.render();
-        assert_eq!(s, "hello world, foo");
-    }
-
-}
