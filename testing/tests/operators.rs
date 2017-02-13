a6b4f305e948cdf327b45abe52b27a4d81013d5d
Dirkjan Ochtman <dirkjan@ochtman.nl>
2017-02-13 21:55:47 +0100

    Rename comparison tests to be about any operators

--- testing/tests/operators.rs
+++ testing/tests/operators.rs
@@ -5,15 +5,15 @@ extern crate askama_derive;
 use askama::Template;
 
 #[derive(Template)]
-#[template(path = "comparison.html")]
-struct ComparisonTemplate {
+#[template(path = "operators.html")]
+struct OperatorsTemplate {
     a: usize,
     b: usize,
     c: usize,
 }
 
 #[test]
-fn test_comparison() {
-    let t = ComparisonTemplate { a: 1, b: 1, c: 2 };
+fn test_operators() {
+    let t = OperatorsTemplate { a: 1, b: 1, c: 2 };
     assert_eq!(t.render(), "tf\ntf\ntf\ntf\ntf\ntf\n");
 }
