0eeb5d1b2d998f3ea56b5682f618a57443cd8aef
Dirkjan Ochtman <dirkjan@ochtman.nl>
2017-03-07 21:00:44 +0100

    Add test for nested loops and loops over slices (see #6, #7, #9)

--- testing/tests/loops.rs
+++ testing/tests/loops.rs
@@ -16,3 +16,20 @@ fn test_for() {
     };
     assert_eq!(s.render(), "0. A\n1. alfa\n2. 1\n");
 }
+
+
+#[derive(Template)]
+#[template(path = "nested-for.html", print = "code")]
+struct NestedForTemplate<'a> {
+    seqs: Vec<&'a [&'a str]>,
+}
+
+#[test]
+fn test_nested_for() {
+    let alpha = vec!["a", "b", "c"];
+    let numbers = vec!["one", "two"];
+    let s = NestedForTemplate {
+        seqs: vec![&alpha, &numbers],
+    };
+    assert_eq!(s.render(), "1\n  0a1b2c2\n  0one1two");
+}
