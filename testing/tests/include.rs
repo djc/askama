549d0ec3c6799eb31517b093faa7c85752ba7d8c
Dirkjan Ochtman <dirkjan@ochtman.nl>
2017-08-04 13:19:31 +0200

    Add test for include blocks (see #25)

---
+++ testing/tests/include.rs
@@ -0,0 +1,19 @@
+#[macro_use]
+extern crate askama;
+
+use askama::Template;
+
+#[derive(Template)]
+#[template(path = "include.html")]
+struct IncludeTemplate<'a> {
+    strs: &'a [&'a str],
+}
+
+#[test]
+fn test_include() {
+    let strs = vec!["foo", "bar"];
+    let s = IncludeTemplate {
+        strs: &strs,
+    };
+    assert_eq!(s.render(), "INCLUDED: fooINCLUDED: bar")
+}
