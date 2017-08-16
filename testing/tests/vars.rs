d7ac0825e0759cde171670175a5e129ad1f2af10
Dirkjan Ochtman <dirkjan@ochtman.nl>
2017-08-16 12:51:11 +0200

    Add test for variable declaration

--- testing/tests/vars.rs
+++ testing/tests/vars.rs
@@ -14,3 +14,17 @@ fn test_let() {
     let t = LetTemplate { s: "foo" };
     assert_eq!(t.render().unwrap(), "foo");
 }
+
+
+#[derive(Template)]
+#[template(path = "let-decl.html")]
+struct LetDeclTemplate<'a> {
+    cond: bool,
+    s: &'a str,
+}
+
+#[test]
+fn test_let_decl() {
+    let t = LetDeclTemplate { cond: false, s: "bar" };
+    assert_eq!(t.render().unwrap(), "bar");
+}
