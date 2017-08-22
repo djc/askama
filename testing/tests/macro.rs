d0a3d51dcda1f4c0509f9e083e3d02c81d23776a
Dirkjan Ochtman <dirkjan@ochtman.nl>
2017-08-22 20:22:33 +0200

    Add test for basic macro use

---
+++ testing/tests/macro.rs
@@ -0,0 +1,16 @@
+#[macro_use]
+extern crate askama;
+
+use askama::Template;
+
+#[derive(Template)]
+#[template(path = "macro.html", print = "all")]
+struct MacroTemplate<'a> {
+    s: &'a str,
+}
+
+#[test]
+fn test_macro() {
+    let t = MacroTemplate { s: "foo" };
+    assert_eq!(t.render().unwrap(), "foo foo foo");
+}
