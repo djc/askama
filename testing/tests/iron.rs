40775d8c885bda6bd8e086bcc03127c714fc890e
Dirkjan Ochtman <dirkjan@ochtman.nl>
2017-08-23 20:43:11 +0200

    Add test for Iron support

---
+++ testing/tests/iron.rs
@@ -0,0 +1,20 @@
+#[macro_use]
+extern crate askama;
+extern crate iron;
+
+use askama::Template;
+use iron::{status, Response};
+
+#[derive(Template)]
+#[template(path = "hello.html")]
+struct HelloTemplate<'a> {
+    name: &'a str,
+}
+
+#[test]
+fn test_iron() {
+    let rsp = Response::with((status::Ok, HelloTemplate { name: "world" }));
+    let mut buf = Vec::new();
+    let _ = rsp.body.unwrap().write_body(&mut buf);
+    assert_eq!(buf, "Hello, world!".as_bytes());
+}
