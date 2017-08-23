e0300661068f3c8141cdc90fa915f7c61ce3d1ab
Dirkjan Ochtman <dirkjan@ochtman.nl>
2017-08-23 16:32:28 +0200

    Add test for Rocket support

---
+++ testing/tests/rocket.rs
@@ -0,0 +1,33 @@
+#![cfg(feature = "rocket")]
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+#[macro_use]
+extern crate askama;
+extern crate rocket;
+
+use askama::Template;
+
+use rocket::local::Client;
+use rocket::http::{ContentType, Status};
+
+#[derive(Template)]
+#[template(path = "hello.html")]
+struct HelloTemplate<'a> {
+    name: &'a str,
+}
+
+#[get("/")]
+fn hello() -> HelloTemplate<'static> {
+    HelloTemplate { name: "world" }
+}
+
+#[test]
+fn test_rocket() {
+    let rocket = rocket::ignite().mount("/", routes![hello]);
+    let client = Client::new(rocket).unwrap();
+    let mut rsp = client.get("/").dispatch();
+    assert_eq!(rsp.status(), Status::Ok);
+    assert_eq!(rsp.content_type(), Some(ContentType::HTML));
+    assert_eq!(rsp.body_string().unwrap(), "Hello, world!");
+}
