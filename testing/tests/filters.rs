8abf31c1a8389b24040e844f154be37ef2dc5992
defyrlt <defyrlt@users.noreply.github.com>
2017-08-24 22:27:53 +0300

    Enable escaping by default

--- testing/tests/filters.rs
+++ testing/tests/filters.rs
@@ -4,22 +4,6 @@ extern crate askama;
 use askama::Template;
 
 #[derive(Template)]
-#[template(path = "filters.html")]
-struct TestTemplate {
-    strvar: String,
-}
-
-#[test]
-fn filter_escape() {
-    let s = TestTemplate {
-        strvar: "my <html> is unsafe & should be escaped".to_string(),
-    };
-    assert_eq!(s.render().unwrap(),
-               "my &lt;html&gt; is unsafe &amp; should be escaped");
-}
-
-
-#[derive(Template)]
 #[template(path = "format.html")]
 struct FormatTemplate<'a> {
     var: &'a str,
