c8b14c6d0d61e12c45860c279f648ea6904488c3
Dirkjan Ochtman <dirkjan@ochtman.nl>
2017-08-24 20:22:32 +0200

    Add test case for user-defined filters

--- testing/tests/filters.rs
+++ testing/tests/filters.rs
@@ -30,3 +30,22 @@ fn filter_format() {
     let t = FormatTemplate { var: "formatted" };
     assert_eq!(t.render().unwrap(), "\"formatted\"");
 }
+
+
+#[derive(Template)]
+#[template(source = "{{ s|myfilter }}")]
+struct MyFilterTemplate<'a> {
+    s: &'a str,
+}
+
+mod filters {
+    pub fn myfilter(s: &str) -> ::askama::Result<String> {
+        Ok(s.replace("oo", "aa").to_string())
+    }
+}
+
+#[test]
+fn test_my_filter() {
+    let t = MyFilterTemplate { s: "foo" };
+    assert_eq!(t.render().unwrap(), "faa");
+}
