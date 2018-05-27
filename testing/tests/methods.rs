3ba47e3fba811c073fc0623374f15861191d2edf
Dirkjan Ochtman <dirkjan@ochtman.nl>
2018-05-27 10:00:35 +0200

    Reformat and rename method testing tests for better organization

--- testing/tests/methods.rs
+++ testing/tests/methods.rs
@@ -5,38 +5,39 @@ use askama::Template;
 
 #[derive(Template)]
 #[template(source = "{{ self.get_s() }}", ext = "txt")]
-struct MethodTemplate<'a> {
+struct SelfMethodTemplate<'a> {
     s: &'a str,
 }
 
-impl<'a> MethodTemplate<'a> {
+impl<'a> SelfMethodTemplate<'a> {
     fn get_s(&self) -> &str {
         self.s
     }
 }
 
-#[derive(Template)]
-#[template(source = "{{ self.get_s() }} {{ t.get_s() }}", ext = "txt")]
-struct NestedMethodTemplate<'a> {
-    t: MethodTemplate<'a>,
+#[test]
+fn test_self_method() {
+    let t = SelfMethodTemplate { s: "foo" };
+    assert_eq!(t.render().unwrap(), "foo");
 }
 
-impl<'a> NestedMethodTemplate<'a> {
+
+#[derive(Template)]
+#[template(source = "{{ self.get_s() }} {{ t.get_s() }}", ext = "txt")]
+struct NestedSelfMethodTemplate<'a> {
+    t: SelfMethodTemplate<'a>,
+}
+
+impl<'a> NestedSelfMethodTemplate<'a> {
     fn get_s(&self) -> &str {
         "bar"
     }
 }
 
 #[test]
-fn test_method() {
-    let t = MethodTemplate { s: "foo" };
-    assert_eq!(t.render().unwrap(), "foo");
-}
-
-#[test]
 fn test_nested() {
-    let t = NestedMethodTemplate {
-        t: MethodTemplate { s: "foo" },
+    let t = NestedSelfMethodTemplate {
+        t: SelfMethodTemplate { s: "foo" },
     };
     assert_eq!(t.render().unwrap(), "bar foo");
 }
