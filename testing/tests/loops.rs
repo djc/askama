a225bb477f0a2cc4e58c5d9aa9c5874be2f4158c
bott <mhpoin@gmail.com>
2018-09-13 23:40:17 +0200

    Fix operator preference at loop.first

--- testing/tests/loops.rs
+++ testing/tests/loops.rs
@@ -32,3 +32,17 @@ fn test_nested_for() {
     };
     assert_eq!(s.render().unwrap(), "1\n  0a1b2c2\n  0one1two");
 }
+
+#[derive(Template)]
+#[template(path = "precedence-for.html")]
+struct PrecedenceTemplate<'a> {
+    strings: Vec<&'a str>,
+}
+
+#[test]
+fn test_precedence_for() {
+    let s = PrecedenceTemplate {
+        strings: vec!["A", "alfa", "1"],
+    };
+    assert_eq!(s.render().unwrap(), "0. A (first)\n1. alfa\n2. 1\n");
+}
