35e241f429778e5b0060d3455087db37b38c921b
Dirkjan Ochtman <dirkjan@ochtman.nl>
2018-10-08 10:40:20 +0200

    Clean up warning about unused field

--- testing/tests/rust_macro.rs
+++ testing/tests/rust_macro.rs
@@ -4,19 +4,17 @@ extern crate askama;
 use askama::Template;
 
 macro_rules! hello {
-    ($name:expr) => {
+    () => {
         "world"
     }
 }
 
 #[derive(Template)]
 #[template(path = "rust-macros.html")]
-struct RustMacrosTemplate<'a> {
-    name: &'a str,
-}
+struct RustMacrosTemplate {}
 
 #[test]
 fn main() {
-    let template = RustMacrosTemplate { name: "foo" };
+    let template = RustMacrosTemplate {};
     assert_eq!("Hello, world!", template.render().unwrap());
 }
