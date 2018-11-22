f0bfd7322d690be7e347d337344a02ba0712752c
bott <mhpoin@gmail.com>
2018-11-22 19:03:10 +0100

    Add avx2 escape

---
+++ askama_escape/build.rs
@@ -0,0 +1,28 @@
+extern crate version_check;
+
+use std::env;
+
+use version_check::is_min_version;
+
+fn main() {
+    enable_simd_optimizations();
+}
+
+fn enable_simd_optimizations() {
+    if is_env_set("CARGO_CFG_ASKAMA_DISABLE_AUTO_SIMD") {
+        return;
+    }
+    if !is_min_version("1.27.0")
+        .map(|(yes, _)| yes)
+        .unwrap_or(false)
+    {
+        return;
+    }
+
+    println!("cargo:rustc-cfg=askama_runtime_simd");
+    println!("cargo:rustc-cfg=askama_runtime_avx");
+}
+
+fn is_env_set(name: &str) -> bool {
+    env::var(name).is_ok()
+}
--- askama_escape/src/lib.rs
+++ askama_escape/src/lib.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate cfg_if;
+
 use std::fmt::{self, Display, Formatter};
 use std::str;
 
@@ -49,42 +52,297 @@ pub fn escape(s: &str) -> Escaped {
     }
 }
 
-macro_rules! escaping_body {
-    ($start:ident, $i:ident, $fmt:ident, $_self:ident, $quote:expr) => {{
+pub struct Escaped<'a> {
+    bytes: &'a [u8],
+}
+
+impl<'a> Display for Escaped<'a> {
+    fn fmt(&self, fmt: &mut Formatter) -> fmt::Result {
+        _imp(self.bytes, fmt)
+    }
+}
+
+cfg_if! {
+    if #[cfg(all(target_arch = "x86_64", not(target_os = "windows"), askama_runtime_simd))] {
+
+        use std::arch::x86_64::*;
+        use std::mem::{self, size_of};
+        use std::sync::atomic::{AtomicUsize, Ordering};
+
+        #[inline(always)]
+        fn _imp(bytes: &[u8], fmt: &mut Formatter) -> fmt::Result {
+            // https://github.com/BurntSushi/rust-memchr/blob/master/src/x86/mod.rs#L9-L29
+            static mut FN: fn(bytes: &[u8], fmt: &mut Formatter) -> fmt::Result = detect;
+
+            fn detect(bytes: &[u8], fmt: &mut Formatter) -> fmt::Result {
+                let fun = if cfg!(askama_runtime_avx) && is_x86_feature_detected!("avx2") {
+                    _avx_escape as usize
+                } else {
+                    _escape as usize
+                };
+
+                let slot = unsafe { &*(&FN as *const _ as *const AtomicUsize) };
+                slot.store(fun as usize, Ordering::Relaxed);
+                unsafe {
+                    mem::transmute::<usize, fn(bytes: &[u8], fmt: &mut Formatter) -> fmt::Result>(fun)(bytes, fmt)
+                }
+            }
+
+            unsafe {
+                let slot = &*(&FN as *const _ as * const AtomicUsize);
+                let fun = slot.load(Ordering::Relaxed);
+                mem::transmute::<usize, fn(bytes: &[u8], fmt: &mut Formatter) -> fmt::Result>(fun)(bytes, fmt)
+            }
+        }
+
+        // Subtract `b` from `a` and return the difference. `a` should be greater than
+        // or equal to `b`.
+        #[inline(always)]
+        fn sub(a: *const u8, b: *const u8) -> usize {
+            debug_assert!(b <= a);
+            (a as usize) - (b as usize)
+        }
+    } else {
+
+        #[inline(always)]
+        fn _imp(bytes: &[u8], fmt: &mut Formatter) -> fmt::Result {
+            _escape(bytes, fmt)
+        }
+    }
+}
+
+// Main instruction in escape
+// Test 'start' iterator in current position,
+// write slice since start to current position - 1,
+// escape byte with quote and
+// set 'start' iterator with next position
+macro_rules! escape_body {
+    ($i:expr, $start:ident, $fmt:ident, $bytes:ident, $quote:expr) => {{
         if $start < $i {
-            $fmt.write_str(unsafe { str::from_utf8_unchecked(&$_self.bytes[$start..$i]) })?;
+            #[allow(unused_unsafe)]
+            $fmt.write_str(unsafe { str::from_utf8_unchecked(&$bytes[$start..$i]) })?;
         }
         $fmt.write_str($quote)?;
         $start = $i + 1;
     }};
 }
 
-pub struct Escaped<'a> {
-    bytes: &'a [u8],
+// Select between pairs bytes - quote to call the callback
+macro_rules! bodies {
+    ($i:expr, $b:expr, $start:ident, $fmt:ident, $bytes:ident, $callback:ident) => {
+        match $b {
+            b'<' => $callback!($i, $start, $fmt, $bytes, "&lt;"),
+            b'>' => $callback!($i, $start, $fmt, $bytes, "&gt;"),
+            b'&' => $callback!($i, $start, $fmt, $bytes, "&amp;"),
+            b'"' => $callback!($i, $start, $fmt, $bytes, "&quot;"),
+            b'\'' => $callback!($i, $start, $fmt, $bytes, "&#x27;"),
+            b'/' => $callback!($i, $start, $fmt, $bytes, "&#x2f;"),
+            _ => (),
+        }
+    };
 }
 
-impl<'a> ::std::fmt::Display for Escaped<'a> {
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        let mut start = 0;
-        for (i, b) in self.bytes.iter().enumerate() {
-            if b.wrapping_sub(b'"') <= FLAG {
-                match *b {
-                    b'<' => escaping_body!(start, i, fmt, self, "&lt;"),
-                    b'>' => escaping_body!(start, i, fmt, self, "&gt;"),
-                    b'&' => escaping_body!(start, i, fmt, self, "&amp;"),
-                    b'"' => escaping_body!(start, i, fmt, self, "&quot;"),
-                    b'\'' => escaping_body!(start, i, fmt, self, "&#x27;"),
-                    b'/' => escaping_body!(start, i, fmt, self, "&#x2f;"),
-                    _ => (),
+// Wrap the body of the escape over the body of the mask
+// Resolve expression and do the escape body
+#[allow(unused_macros)]
+macro_rules! mask_body {
+    ($i:expr, $start:ident, $fmt:ident, $bytes:ident, $quote:expr) => {{
+        let i = $i;
+        escape_body!(i, $start, $fmt, $bytes, $quote);
+    }};
+}
+
+// Format bytes in the mask that starts in the current pointer
+#[allow(unused_macros)]
+macro_rules! mask_bodies {
+    ($mask:ident, $at:ident, $cur:ident, $ptr:ident, $start:ident, $fmt:ident, $bytes:ident) => {
+        bodies!($at + $cur, *$ptr.add($cur), $start, $fmt, $bytes, mask_body);
+
+        $mask ^= 1 << $cur;
+        if $mask == 0 {
+            break;
+        }
+
+        $cur = $mask.trailing_zeros() as usize;
+    };
+}
+
+// Write a mask
+// Select starts mask byte pointer and current position to deep find.
+// The main loop break when mask == 0
+#[allow(unused_macros)]
+macro_rules! write_mask {
+    ($mask:ident, $ptr:ident, $start_ptr:ident, $start:ident, $fmt:ident, $bytes:ident) => {{
+        let at = sub($ptr, $start_ptr);
+        let mut cur = $mask.trailing_zeros() as usize;
+
+        loop {
+            mask_bodies!($mask, at, cur, $ptr, $start, $fmt, $bytes);
+        }
+
+        debug_assert_eq!(at, sub($ptr, $start_ptr))
+    }};
+}
+
+/// Scalar html escape
+fn _escape(bytes: &[u8], fmt: &mut Formatter) -> fmt::Result {
+    let mut start = 0;
+
+    for (i, b) in bytes.iter().enumerate() {
+        if b.wrapping_sub(FLAG_BELOW) <= LEN {
+            bodies!(i, *b, start, fmt, bytes, escape_body);
+        }
+    }
+
+    fmt.write_str(unsafe { str::from_utf8_unchecked(&bytes[start..]) })?;
+
+    Ok(())
+}
+#[cfg(all(
+    target_arch = "x86_64",
+    not(target_os = "windows"),
+    askama_runtime_simd,
+    askama_runtime_avx
+))]
+#[target_feature(enable = "avx2")]
+unsafe fn _avx_escape(bytes: &[u8], fmt: &mut Formatter) -> fmt::Result {
+    const VECTOR_SIZE: usize = size_of::<__m256i>();
+    const VECTOR_ALIGN: usize = VECTOR_SIZE - 1;
+    const LOOP_SIZE: usize = 4 * VECTOR_SIZE;
+
+    let v_flag = _mm256_set1_epi8((LEN + 1) as i8);
+    let v_flag_below = _mm256_set1_epi8(FLAG_BELOW as i8);
+
+    let len = bytes.len();
+    let start_ptr = bytes.as_ptr();
+    let mut ptr = start_ptr;
+    let mut start = 0;
+
+    // Write a sliced mask
+    macro_rules! write_forward {
+        ($mask: ident, $align:ident) => {{
+            if $mask != 0 {
+                let at = sub(ptr, start_ptr);
+                let mut cur = $mask.trailing_zeros() as usize;
+
+                while cur < $align {
+                    mask_bodies!($mask, at, cur, ptr, start, fmt, bytes);
                 }
+
+                debug_assert_eq!(at, sub(ptr, start_ptr))
+            }
+        }};
+    }
+
+    if len < VECTOR_SIZE {
+        let a = _mm256_loadu_si256(ptr as *const __m256i);
+        let cmp = _mm256_cmpgt_epi8(v_flag, _mm256_sub_epi8(a, v_flag_below));
+        let mut mask = _mm256_movemask_epi8(cmp);
+
+        write_forward!(mask, len);
+    } else {
+        let end_ptr = bytes[len..].as_ptr();
+
+        {
+            let align = (VECTOR_SIZE - (start_ptr as usize & VECTOR_ALIGN)) & VECTOR_ALIGN;
+            if 0 < align {
+                let a = _mm256_loadu_si256(ptr as *const __m256i);
+                let cmp = _mm256_cmpgt_epi8(v_flag, _mm256_sub_epi8(a, v_flag_below));
+                let mut mask = _mm256_movemask_epi8(cmp);
+
+                write_forward!(mask, align);
+                ptr = ptr.add(align);
+
+                debug_assert!(start <= sub(ptr, start_ptr));
             }
         }
-        fmt.write_str(unsafe { str::from_utf8_unchecked(&self.bytes[start..]) })?;
-        Ok(())
+
+        debug_assert!(start_ptr <= ptr && start_ptr <= end_ptr.sub(VECTOR_SIZE));
+
+        if LOOP_SIZE <= len {
+            // Main loop 128 bytes, need aligned ptr at VECTOR_SIZE
+            while ptr <= end_ptr.sub(LOOP_SIZE) {
+                // Need aligned
+                debug_assert_eq!(0, (ptr as usize) % VECTOR_SIZE);
+
+                let a = _mm256_load_si256(ptr as *const __m256i);
+                let b = _mm256_load_si256(ptr.add(VECTOR_SIZE) as *const __m256i);
+                let c = _mm256_load_si256(ptr.add(VECTOR_SIZE * 2) as *const __m256i);
+                let d = _mm256_load_si256(ptr.add(VECTOR_SIZE * 3) as *const __m256i);
+                let cmp_a = _mm256_cmpgt_epi8(v_flag, _mm256_sub_epi8(a, v_flag_below));
+                let cmp_b = _mm256_cmpgt_epi8(v_flag, _mm256_sub_epi8(b, v_flag_below));
+                let cmp_c = _mm256_cmpgt_epi8(v_flag, _mm256_sub_epi8(c, v_flag_below));
+                let cmp_d = _mm256_cmpgt_epi8(v_flag, _mm256_sub_epi8(d, v_flag_below));
+                let or1 = _mm256_or_si256(cmp_a, cmp_b);
+                let or2 = _mm256_or_si256(cmp_c, cmp_d);
+
+                // Adjust the four masks in two from right to left.
+                if _mm256_movemask_epi8(_mm256_or_si256(or1, or2)) != 0 {
+                    let mut mask = _mm256_movemask_epi8(cmp_a) as i64
+                        | (_mm256_movemask_epi8(cmp_b) as i64) << VECTOR_SIZE;
+
+                    if mask != 0 {
+                        write_mask!(mask, ptr, start_ptr, start, fmt, bytes);
+                    }
+                    let ptr = ptr.add(VECTOR_SIZE + VECTOR_SIZE);
+
+                    mask = _mm256_movemask_epi8(cmp_b) as i64
+                        | (_mm256_movemask_epi8(cmp_c) as i64) << VECTOR_SIZE;
+
+                    if mask != 0 {
+                        write_mask!(mask, ptr, start_ptr, start, fmt, bytes);
+                    }
+                }
+
+                ptr = ptr.add(LOOP_SIZE);
+
+                debug_assert!(start <= sub(ptr, start_ptr));
+            }
+        }
+
+        while ptr <= end_ptr.sub(VECTOR_SIZE) {
+            // Need aligned
+            debug_assert_eq!(0, (ptr as usize) % VECTOR_SIZE);
+
+            let a = _mm256_load_si256(ptr as *const __m256i);
+            let cmp = _mm256_cmpgt_epi8(v_flag, _mm256_sub_epi8(a, v_flag_below));
+            let mut mask = _mm256_movemask_epi8(cmp);
+
+            if mask != 0 {
+                write_mask!(mask, ptr, start_ptr, start, fmt, bytes);
+            }
+            ptr = ptr.add(VECTOR_SIZE);
+
+            debug_assert!(start <= sub(ptr, start_ptr));
+        }
+
+        debug_assert!(end_ptr.sub(VECTOR_SIZE) < ptr);
+
+        if ptr < end_ptr {
+            // Need aligned
+            debug_assert_eq!(0, (ptr as usize) % VECTOR_SIZE);
+
+            let a = _mm256_load_si256(ptr as *const __m256i);
+            let cmp = _mm256_cmpgt_epi8(v_flag, _mm256_sub_epi8(a, v_flag_below));
+            let mut mask = _mm256_movemask_epi8(cmp);
+            let end = sub(end_ptr, ptr);
+
+            write_forward!(mask, end);
+        }
     }
+
+    // Write since start to the end of the slice
+    debug_assert!(start <= len);
+    if start < len {
+        fmt.write_str(str::from_utf8_unchecked(&bytes[start..len]))?;
+    }
+
+    Ok(())
 }
 
-const FLAG: u8 = b'>' - b'"';
+// Defining character interval from ASCII table to create bit masks from slice to be escaped
+const LEN: u8 = b'>' - b'"';
+const FLAG_BELOW: u8 = b'"';
 
 #[cfg(test)]
 mod tests {
