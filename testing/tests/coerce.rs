c2901f36e25feadbbb853583183f212cc20fe4b2
vallentin <mail@vallentin.dev>
2020-12-01 14:30:32 +0100

    Added new test case

---
+++ testing/tests/coerce.rs
@@ -0,0 +1,14 @@
+use askama::Template;
+
+#[derive(Template)]
+#[template(path = "if-coerce.html")]
+struct IfCoerceTemplate {
+    t: bool,
+    f: bool,
+}
+
+#[test]
+fn test_coerce() {
+    let t = IfCoerceTemplate { t: true, f: false };
+    assert_eq!(t.render().unwrap(), "ftftfttfttft");
+}
