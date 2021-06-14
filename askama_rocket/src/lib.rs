db165497fc149aa3089f2a1a0b68d2d7d0b60d9f
Shritesh Bhattarai <shr@ite.sh>
2021-06-14 09:12:12 -0400

    Remove redundant lifetime

--- askama_rocket/src/lib.rs
+++ askama_rocket/src/lib.rs
@@ -6,7 +6,7 @@ pub use rocket::request::Request;
 use rocket::response::Response;
 pub use rocket::response::{Responder, Result};
 
-pub fn respond<'r, 'o, 'r, T: Template>(t: &'r T, ext: &'r str) -> Result<'o> {
+pub fn respond<'r, 'o, T: Template>(t: &'r T, ext: &'r str) -> Result<'o> {
     let rsp = t.render().map_err(|_| Status::InternalServerError)?;
     let ctype = ContentType::from_extension(ext).ok_or(Status::InternalServerError)?;
     Response::build()
