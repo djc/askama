939c5a2c79b6231cfff41656a2b2971ef97a1fe0
René Kijewski <kijewski@library.vetmed.fu-berlin.de>
2022-01-05 19:35:53 +0100

    No needless boxing of the error

--- askama_actix/src/lib.rs
+++ askama_actix/src/lib.rs
@@ -1,10 +1,31 @@
 #![deny(elided_lifetimes_in_paths)]
 
+use std::fmt;
+
 use actix_web::body::BoxBody;
 use actix_web::http::StatusCode;
 use actix_web::{HttpResponse, HttpResponseBuilder, ResponseError};
 use askama::mime::extension_to_mime_type;
 pub use askama::*;
+
+/// Newtype to let askama::Error implement actix_web::ResponseError.
+struct ActixError(Error);
+
+impl fmt::Debug for ActixError {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        <Error as fmt::Debug>::fmt(&self.0, f)
+    }
+}
+
+impl fmt::Display for ActixError {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        <Error as fmt::Display>::fmt(&self.0, f)
+    }
+}
+
+impl ResponseError for ActixError {}
 
 pub trait TemplateToResponse {
     fn to_response(&self) -> HttpResponse<BoxBody>;
@@ -19,9 +40,7 @@ impl<T: askama::Template> TemplateToResponse for T {
                     .content_type(ctype)
                     .body(buffer)
             }
-            Err(err) => {
-                HttpResponse::from_error(Box::new(err) as Box<dyn std::error::Error + 'static>)
-            }
+            Err(err) => HttpResponse::from_error(ActixError(err)),
         }
     }
 }
