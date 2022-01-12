48c1bf7d192cc5d2e423eb3036cc172297fc8dc3
René Kijewski <kijewski@library.vetmed.fu-berlin.de>
2022-01-12 16:21:14 +0100

    `&Option<T>` → `Option<&T>`

--- askama_derive/src/lib.rs
+++ askama_derive/src/lib.rs
@@ -65,7 +65,7 @@ fn build_template(ast: &syn::DeriveInput) -> Result<String, CompileError> {
         eprintln!("{:?}", parsed[&input.path]);
     }
 
-    let code = generator::generate(&input, &contexts, &heritage, INTEGRATIONS)?;
+    let code = generator::generate(&input, &contexts, heritage.as_ref(), INTEGRATIONS)?;
     if input.print == Print::Code || input.print == Print::All {
         eprintln!("{}", code);
     }
