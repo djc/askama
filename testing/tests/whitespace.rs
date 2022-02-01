19b1a35c0436403461b4baf73dded3b5841f7aa3
René Kijewski <kijewski@library.vetmed.fu-berlin.de>
2022-02-01 15:32:37 +0100

    Make json filter safe
    Previously the built-in json filter had an issue that made it unsafe to
    use in HTML data. When used in HTML attributes an attacker who is able
    to supply an arbitrary string that should be JSON encoded could close
    the containing HTML element e.g. with `"</div>"`, and write arbitrary
    HTML code afterwards as long as they use apostrophes instead of
    quotation marks. The programmer could make this use case safe by
    explicitly escaping the JSON result: `{{data|json|escape}}`.
    
    In a `<script>` context the json filter was not usable at all, because
    in scripts HTML escaped entities are not parsed outside of XHTML
    documents. Without using the safe filter an attacker could close the
    current script using `"</script>"`.
    
    This PR fixes the problem by always escaping less-than, greater-than,
    ampersand, and apostrophe characters using their JSON unicode escape
    sequence `\u00xx`. Unless the programmer explicitly uses the safe
    filter, quotation marks are HTML encoded as `&quot`. In scripts the
    programmer should use the safe filter, otherwise not.

--- testing/tests/whitespace.rs
+++ testing/tests/whitespace.rs
@@ -39,9 +39,5 @@ fn test_extra_whitespace() {
     let mut template = AllowWhitespaces::default();
     template.nested_1.nested_2.array = &["a0", "a1", "a2", "a3"];
     template.nested_1.nested_2.hash.insert("key", "value");
-<<<<<<< HEAD
-    assert_eq!(template.render().unwrap(), "\n0\n0\n0\n0\n\n\n\n0\n0\n0\n0\n0\n\na0\na1\nvalue\n\n\n\n\n\n[\n  \"a0\",\n  \"a1\",\n  \"a2\",\n  \"a3\"\n]\n[\n  \"a0\",\n  \"a1\",\n  \"a2\",\n  \"a3\"\n][\n  \"a0\",\n  \"a1\",\n  \"a2\",\n  \"a3\"\n]\n[\n  \"a1\"\n][\n  \"a1\"\n]\n[\n  \"a1\",\n  \"a2\"\n][\n  \"a1\",\n  \"a2\"\n]\n[\n  \"a1\"\n][\n  \"a1\"\n]1-1-1\n3333 3\n2222 2\n0000 0\n3333 3\n\ntruefalse\nfalsefalsefalse\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-=======
     assert_eq!(template.render().unwrap(), "\n0\n0\n0\n0\n\n\n\n0\n0\n0\n0\n0\n\na0\na1\nvalue\n\n\n\n\n\n[\n  &quot;a0&quot;,\n  &quot;a1&quot;,\n  &quot;a2&quot;,\n  &quot;a3&quot;\n]\n[\n  &quot;a0&quot;,\n  &quot;a1&quot;,\n  &quot;a2&quot;,\n  &quot;a3&quot;\n][\n  &quot;a0&quot;,\n  &quot;a1&quot;,\n  &quot;a2&quot;,\n  &quot;a3&quot;\n]\n[\n  &quot;a1&quot;\n][\n  &quot;a1&quot;\n]\n[\n  &quot;a1&quot;,\n  &quot;a2&quot;\n][\n  &quot;a1&quot;,\n  &quot;a2&quot;\n]\n[\n  &quot;a1&quot;\n][\n  &quot;a1&quot;\n]1-1-1\n3333 3\n2222 2\n0000 0\n3333 3\n\ntruefalse\nfalsefalsefalse\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
->>>>>>> 29f0c06 (Make json filter safe)
 }
