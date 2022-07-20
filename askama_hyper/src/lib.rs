e081c6f6c3ad24826475118bf43ceca5a4ee1fc1
René Kijewski <kijewski@library.vetmed.fu-berlin.de>
2022-07-20 21:18:18 +0200

    Implement basic hyper integration
    The integration is based on askama_gotham.
    
    There is no specific trait to convert an arbitrary T to
    `hyper::Response`, so I used `From<Template> for hyper::Response`.

---
+++ askama_hyper/src/lib.rs
@@ -0,0 +1,29 @@
+#![forbid(unsafe_code)]
+#![deny(elided_lifetimes_in_paths)]
+#![deny(unreachable_pub)]
+
+pub use askama::*;
+#[doc(hidden)]
+pub use hyper;
+use hyper::{header, Body, Response, StatusCode};
+
+pub fn try_respond<T: Template>(t: &T, _ext: &str) -> Result<Response<Body>> {
+    Response::builder()
+        .status(StatusCode::OK)
+        .header(
+            header::CONTENT_TYPE,
+            header::HeaderValue::from_static(T::MIME_TYPE),
+        )
+        .body(t.render()?.into())
+        .map_err(|err| Error::Custom(Box::new(err)))
+}
+
+pub fn respond<T: Template>(t: &T, _ext: &str) -> Response<Body> {
+    match try_respond(t, _ext) {
+        Ok(response) => response,
+        Err(_) => Response::builder()
+            .status(StatusCode::INTERNAL_SERVER_ERROR)
+            .body(Body::empty())
+            .unwrap(),
+    }
+}
