da6ac753294e794f337cd02c9f59b0b5144920e3
Dominic <git@msrd0.de>
2022-07-31 18:12:17 +0200

    Add failing test case for #708

--- testing/tests/macro.rs
+++ testing/tests/macro.rs
@@ -1,4 +1,5 @@
 use askama::Template;
+use std::collections::BTreeSet;
 
 #[derive(Template)]
 #[template(path = "macro.html")]
@@ -72,4 +73,14 @@ struct StrCmpTemplate;
 fn str_cmp() {
     let t = StrCmpTemplate;
     assert_eq!(t.render().unwrap(), "AfooBotherCneitherD");
+}
+
+#[derive(Template)]
+#[template(path = "macro-iter.html.j2")]
+struct MacroIterTemplate {
+    foos: Vec<Foo>,
+}
+
+struct Foo {
+    list: BTreeSet<String>,
 }
