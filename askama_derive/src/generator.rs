1f3ba0ecb47c87ea9fce9e4ab312e11eed431cb8
Dominic <git@msrd0.de>
2022-08-06 15:00:49 +0200

    always take reference when accessing value passed as argument to a macro

--- askama_derive/src/generator.rs
+++ askama_derive/src/generator.rs
@@ -2034,7 +2034,7 @@ impl MapChain<'_, &str, LocalMeta> {
         let name = normalize_identifier(name);
         self.get(&name).map(|meta| match &meta.refs {
             Some(expr) => expr.clone(),
-            None => name.to_string(),
+            None => format!("&{name}"),
         })
     }
 
