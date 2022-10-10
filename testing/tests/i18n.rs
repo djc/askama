92fadc9bba0c607c48078a7b22d6e64d9da0051c
LeoniePhiline <22329650+LeoniePhiline@users.noreply.github.com>
2022-10-10 16:41:11 +0200

    fix: Remove commented out i18n tests
    Fixes thread https://github.com/djc/askama/pull/700/files#r934558288

--- testing/tests/i18n.rs
+++ testing/tests/i18n.rs
@@ -4,16 +4,6 @@ use askama::{langid, Locale, Template};
 
 askama::localization!(LOCALES);
 
-/*
-#[derive(Template)]
-#[template(path = "i18n_invalid.html")]
-struct UsesI18nInvalid<'a> {
-    #[locale]
-    loc: Locale<'a>,
-    name: &'a str,
-}
-*/
-
 #[derive(Template)]
 #[template(path = "i18n.html")]
 struct UsesI18n<'a> {
@@ -29,16 +19,6 @@ struct UsesNoArgsI18n<'a> {
     #[locale]
     loc: Locale<'a>,
 }
-
-/*
-#[derive(Template)]
-#[template(path = "i18n_broken.html")]
-struct InvalidI18n<'a> {
-    #[locale]
-    loc: Locale<'a>,
-    car_color: &'a str,
-}
-*/
 
 #[test]
 fn existing_language() {
@@ -75,17 +55,3 @@ fn no_args() {
     };
     assert_eq!(template.render().unwrap(), r#"<h3>This is a test</h3>"#)
 }
-
-/*
-#[test]
-fn invalid_tags_language() {
-    let template = InvalidI18n {
-        loc: Locale::new(langid!("nl-BE"), &LOCALES),
-        car_color: "Red",
-    };
-    assert_eq!(
-        template.render().unwrap(),
-        r#"<h1>Unknown localization car</h1>"#
-    );
-}
-*/
