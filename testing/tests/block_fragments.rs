5ced368215cdcafb20258dd240ed92d4c04fdba3
wrapperup <wrapperup4@gmail.com>
2023-07-01 11:15:21 -0400

    move types with corresponding test

--- testing/tests/block_fragments.rs
+++ testing/tests/block_fragments.rs
@@ -1,36 +1,9 @@
 use askama::Template;
 
-// fragment-nested-block.html
-// fragment-nested-super.html
-// fragment-simple.html
-// fragment-super.html
-
 #[derive(Template)]
 #[template(path = "fragment-simple.html", block = "body")]
 struct FragmentSimple<'a> {
     name: &'a str,
-}
-
-#[derive(Template)]
-#[template(path = "fragment-super.html", block = "body")]
-struct FragmentSuper<'a> {
-    name: &'a str,
-}
-
-#[derive(Template)]
-#[template(path = "fragment-nested-block.html", block = "nested")]
-struct FragmentNestedBlock;
-
-#[derive(Template)]
-#[template(path = "fragment-nested-super.html", block = "body")]
-struct FragmentNestedSuper<'a> {
-    name: &'a str,
-}
-
-#[derive(Template)]
-#[template(path = "fragment-unused-expr.html", block = "body")]
-struct FragmentUnusedExpr<'a> {
-    required: &'a str,
 }
 
 /// Tests a simple base-inherited template with block fragment rendering.
@@ -39,6 +12,12 @@ fn test_fragment_simple() {
     let simple = FragmentSimple { name: "world" };
 
     assert_eq!(simple.render().unwrap(), "\n\n<p>Hello world!</p>\n");
+}
+
+#[derive(Template)]
+#[template(path = "fragment-super.html", block = "body")]
+struct FragmentSuper<'a> {
+    name: &'a str,
 }
 
 /// Tests a case where a block fragment rendering calls the parent.
@@ -53,6 +32,10 @@ fn test_fragment_super() {
     );
 }
 
+#[derive(Template)]
+#[template(path = "fragment-nested-block.html", block = "nested")]
+struct FragmentNestedBlock;
+
 /// Tests rendering a block fragment inside of a block.
 #[test]
 fn test_fragment_nested_block() {
@@ -62,6 +45,12 @@ fn test_fragment_nested_block() {
         nested_block.render().unwrap(),
         "\n\n<p>I should be here.</p>\n"
     );
+}
+
+#[derive(Template)]
+#[template(path = "fragment-nested-super.html", block = "body")]
+struct FragmentNestedSuper<'a> {
+    name: &'a str,
 }
 
 /// Tests rendering a block fragment with multiple inheritance.
@@ -74,6 +63,12 @@ fn test_fragment_nested_super() {
         nested_sup.render().unwrap(),
         "\n\n<p>Hello world!</p>\n\n[\n<p>Parent body content</p>\n]\n\n"
     );
+}
+
+#[derive(Template)]
+#[template(path = "fragment-unused-expr.html", block = "body")]
+struct FragmentUnusedExpr<'a> {
+    required: &'a str,
 }
 
 /// Tests a case where an expression is defined outside of a block fragment
