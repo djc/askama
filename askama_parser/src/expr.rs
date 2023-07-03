7488e1fd4a0729445698af5fc7d0a75172c5780c
Dirkjan Ochtman <dirkjan@ochtman.nl>
2023-07-03 10:14:23 +0200

    parser: move Expr parser helpers into callers

--- askama_parser/src/expr.rs
+++ askama_parser/src/expr.rs
@@ -102,6 +102,12 @@ impl<'a> Expr<'a> {
     expr_prec_layer!(muldivmod, filtered, "*", "/", "%");
 
     fn filtered(i: &'a str) -> IResult<&'a str, Self> {
+        fn filter(i: &str) -> IResult<&str, (&str, Option<Vec<Expr<'_>>>)> {
+            let (i, (_, fname, args)) =
+                tuple((char('|'), ws(identifier), opt(Expr::arguments)))(i)?;
+            Ok((i, (fname, args)))
+        }
+
         let (i, (obj, filters)) = tuple((Self::prefix, many0(filter)))(i)?;
 
         let mut res = obj;
@@ -242,6 +248,54 @@ impl<'a> Suffix<'a> {
     }
 
     fn r#macro(i: &'a str) -> IResult<&'a str, Self> {
+        fn nested_parenthesis(i: &str) -> IResult<&str, ()> {
+            let mut nested = 0;
+            let mut last = 0;
+            let mut in_str = false;
+            let mut escaped = false;
+
+            for (i, b) in i.chars().enumerate() {
+                if !(b == '(' || b == ')') || !in_str {
+                    match b {
+                        '(' => nested += 1,
+                        ')' => {
+                            if nested == 0 {
+                                last = i;
+                                break;
+                            }
+                            nested -= 1;
+                        }
+                        '"' => {
+                            if in_str {
+                                if !escaped {
+                                    in_str = false;
+                                }
+                            } else {
+                                in_str = true;
+                            }
+                        }
+                        '\\' => {
+                            escaped = !escaped;
+                        }
+                        _ => (),
+                    }
+                }
+
+                if escaped && b != '\\' {
+                    escaped = false;
+                }
+            }
+
+            if nested == 0 {
+                Ok((&i[last..], ()))
+            } else {
+                Err(nom::Err::Error(error_position!(
+                    i,
+                    ErrorKind::SeparatedNonEmptyList
+                )))
+            }
+        }
+
         preceded(
             pair(ws(char('!')), char('(')),
             cut(terminated(
@@ -275,57 +329,4 @@ impl<'a> Suffix<'a> {
     fn r#try(i: &'a str) -> IResult<&'a str, Self> {
         map(preceded(take_till(not_ws), char('?')), |_| Self::Try)(i)
     }
-}
-
-fn nested_parenthesis(i: &str) -> IResult<&str, ()> {
-    let mut nested = 0;
-    let mut last = 0;
-    let mut in_str = false;
-    let mut escaped = false;
-
-    for (i, b) in i.chars().enumerate() {
-        if !(b == '(' || b == ')') || !in_str {
-            match b {
-                '(' => nested += 1,
-                ')' => {
-                    if nested == 0 {
-                        last = i;
-                        break;
-                    }
-                    nested -= 1;
-                }
-                '"' => {
-                    if in_str {
-                        if !escaped {
-                            in_str = false;
-                        }
-                    } else {
-                        in_str = true;
-                    }
-                }
-                '\\' => {
-                    escaped = !escaped;
-                }
-                _ => (),
-            }
-        }
-
-        if escaped && b != '\\' {
-            escaped = false;
-        }
-    }
-
-    if nested == 0 {
-        Ok((&i[last..], ()))
-    } else {
-        Err(nom::Err::Error(error_position!(
-            i,
-            ErrorKind::SeparatedNonEmptyList
-        )))
-    }
-}
-
-fn filter(i: &str) -> IResult<&str, (&str, Option<Vec<Expr<'_>>>)> {
-    let (i, (_, fname, args)) = tuple((char('|'), ws(identifier), opt(Expr::arguments)))(i)?;
-    Ok((i, (fname, args)))
 }
