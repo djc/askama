2f9a041a324a931d914179ec0848dab28a8e7590
Tait Hoyem <tait@tait.tech>
2023-07-30 12:59:00 -0600

    Remove unsafe, unused code

--- askama/src/i18n.rs
+++ askama/src/i18n.rs
@@ -48,7 +48,6 @@ pub use askama_derive::i18n_load as load;
 
 pub use fluent_templates::{self, fluent_bundle::FluentValue, fs::langid, LanguageIdentifier};
 use fluent_templates::{Loader, StaticLoader};
-use parking_lot::const_mutex;
 
 pub struct Locale<'a> {
     loader: &'a StaticLoader,
@@ -71,59 +70,5 @@ impl Locale<'_> {
             false => Some(&args),
         };
         self.loader.lookup_complete(&self.language, msg_id, args)
-    }
-}
-
-/// Similar to OnceCell, but it has an additional take() function, which can only be used once,
-/// and only if the instance was never dereferenced.
-///
-/// The struct is only meant to be used by the [`i18n_load!()`] macro.
-/// Concurrent access will deliberately panic.
-///
-/// Rationale: StaticLoader cannot be cloned.
-#[doc(hidden)]
-pub struct Unlazy<T>(parking_lot::Mutex<UnlazyEnum<T>>);
-
-enum UnlazyEnum<T> {
-    Generator(Option<fn() -> T>),
-    Value(Box<T>),
-}
-
-impl<T> Unlazy<T> {
-    pub const fn new(f: fn() -> T) -> Self {
-        Self(const_mutex(UnlazyEnum::Generator(Some(f))))
-    }
-
-    pub fn take(&self) -> T {
-        let f = match &mut *self.0.try_lock().unwrap() {
-            UnlazyEnum::Generator(f) => f.take(),
-            _ => None,
-        };
-        f.unwrap()()
-    }
-}
-
-impl<T> std::ops::Deref for Unlazy<T>
-where
-    Self: 'static,
-{
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        let data = &mut *self.0.try_lock().unwrap();
-        let value: &T = match data {
-            UnlazyEnum::Generator(f) => {
-                *data = UnlazyEnum::Value(Box::new(f.take().unwrap()()));
-                match data {
-                    UnlazyEnum::Value(value) => value,
-                    _ => unreachable!(),
-                }
-            }
-            UnlazyEnum::Value(value) => value,
-        };
-
-        // SAFETY: This transmutation is safe because once a value is assigned,
-        //         it won't be unassigned again, and Self has static lifetime.
-        unsafe { std::mem::transmute(value) }
     }
 }
