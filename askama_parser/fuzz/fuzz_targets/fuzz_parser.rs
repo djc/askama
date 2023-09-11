8b9a36e87bc26346d0fab36b6ed7c4dbbb95a9db
manunio <max.manu.nair@gmail.com>
2023-09-11 18:21:59 +0530

    fuzz: Add fuzz for askama_parser

---
+++ askama_parser/fuzz/fuzz_targets/fuzz_parser.rs
@@ -0,0 +1,13 @@
+#![no_main]
+use askama_parser::*;
+use libfuzzer_sys::fuzz_target;
+use std::str;
+
+fuzz_target!(|data: &[u8]| {
+    // fuzzed code goes here
+    if data.len() < 500 {
+        if let Ok(data) = str::from_utf8(data) {
+            if let Ok(_) = Ast::from_str(data, &Syntax::default()) {}
+        }
+    }
+});
