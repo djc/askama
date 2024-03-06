5f2b6133ac3397b0a0f076c892e36eb26649213c
René Kijewski <rene.kijewski@fu-berlin.de>
2024-03-06 21:51:21 +0100

    axum: add `(try_)into_response()`

--- askama_axum/src/lib.rs
+++ askama_axum/src/lib.rs
@@ -2,20 +2,27 @@
 #![deny(elided_lifetimes_in_paths)]
 #![deny(unreachable_pub)]
 
+#[doc(no_inline)]
 pub use askama::*;
-pub use axum_core::response::{IntoResponse, Response};
-use http::StatusCode;
+#[doc(no_inline)]
+pub use axum_core;
+use axum_core::response::{IntoResponse, Response};
 
-pub fn into_response<T: Template>(t: &T) -> Response {
-    match t.render() {
-        Ok(body) => {
-            let headers = [(
-                http::header::CONTENT_TYPE,
-                http::HeaderValue::from_static(T::MIME_TYPE),
-            )];
+/// Render a [`Template`] into a [`Response`], or render an error page.
+pub fn into_response<T: ?Sized + askama::Template>(tmpl: &T) -> Response {
+    try_into_response(tmpl)
+        .map_err(|err| axum_core::response::ErrorResponse::from(err.to_string()))
+        .into_response()
+}
 
-            (headers, body).into_response()
-        }
-        Err(_) => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
-    }
+/// Try to render a [`Template`] into a [`Response`].
+pub fn try_into_response<T: ?Sized + askama::Template>(tmpl: &T) -> Result<Response, Error> {
+    let value = tmpl.render()?.into();
+    Response::builder()
+        .header(
+            http::header::CONTENT_TYPE,
+            http::header::HeaderValue::from_static(T::MIME_TYPE),
+        )
+        .body(value)
+        .map_err(|err| Error::Custom(err.into()))
 }
