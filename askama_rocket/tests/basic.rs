40d8c02906b52095cd2400ed0c82fefc8c4c077d
René Kijewski <rene.kijewski@fu-berlin.de>
2024-04-06 18:52:22 +0200

    Slim down feature use in integration crates

--- askama_rocket/tests/basic.rs
+++ askama_rocket/tests/basic.rs
@@ -1,5 +1,4 @@
 use askama_rocket::Template;
-use futures_lite::future::block_on;
 use rocket::http::{ContentType, Status};
 use rocket::local::asynchronous::Client;
 use rocket::Responder;
@@ -20,18 +19,16 @@ fn hello() -> HelloTemplate<'static> {
     HelloTemplate { name: "world" }
 }
 
-#[test]
-fn test_rocket() {
-    block_on(async {
-        let rocket = rocket::build()
-            .mount("/", rocket::routes![hello])
-            .ignite()
-            .await
-            .unwrap();
-        let client = Client::untracked(rocket).await.unwrap();
-        let rsp = client.get("/").dispatch().await;
-        assert_eq!(rsp.status(), Status::Ok);
-        assert_eq!(rsp.content_type(), Some(ContentType::HTML));
-        assert_eq!(rsp.into_string().await.as_deref(), Some("Hello, world!"));
-    });
+#[tokio::test]
+async fn test_rocket() {
+    let rocket = rocket::build()
+        .mount("/", rocket::routes![hello])
+        .ignite()
+        .await
+        .unwrap();
+    let client = Client::untracked(rocket).await.unwrap();
+    let rsp = client.get("/").dispatch().await;
+    assert_eq!(rsp.status(), Status::Ok);
+    assert_eq!(rsp.content_type(), Some(ContentType::HTML));
+    assert_eq!(rsp.into_string().await.as_deref(), Some("Hello, world!"));
 }
