c12339bb574d23a211756ba9c383a997b2abfa4c
René Kijewski <rene.kijewski@fu-berlin.de>
2024-04-18 00:07:17 +0200

    Make `|tojson` filter faster
    This PR changes the filter `|tojson` so that it does not collect the
    serialized data into a string, but writes the data into the target
    writer directly.
    
    This makes the benchmark run 81% (only serializing) or 39% (serializing
    and escaping for HTML) faster. The benchmarked data is not a fair
    representation for the data you would most likely serialize, though.

--- askama/src/filters/json.rs
+++ askama/src/filters/json.rs
@@ -1,5 +1,6 @@
-use crate::error::{Error, Result};
-use askama_escape::JsonEscapeBuffer;
+use std::convert::Infallible;
+use std::{fmt, io, str};
+
 use serde::Serialize;
 use serde_json::to_writer_pretty;
 
@@ -17,10 +18,58 @@ use serde_json::to_writer_pretty;
 /// To use it in HTML attributes, you can either use it in quotation marks `"{{data|json}}"` as is,
 /// or in apostrophes with the (optional) safe filter `'{{data|json|safe}}'`.
 /// In HTML texts the output of e.g. `<pre>{{data|json|safe}}</pre>` is safe, too.
-pub fn json<S: Serialize>(s: S) -> Result<String> {
-    let mut writer = JsonEscapeBuffer::new();
-    to_writer_pretty(&mut writer, &s).map_err(Error::from)?;
-    Ok(writer.finish())
+#[inline]
+pub fn json<S: Serialize>(s: S) -> Result<ToJson<S>, Infallible> {
+    Ok(ToJson(s))
+}
+
+#[derive(Debug, Clone)]
+pub struct ToJson<S: Serialize>(S);
+
+impl<S: Serialize> fmt::Display for ToJson<S> {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        to_writer_pretty(JsonWriter(f), &self.0).map_err(|_| fmt::Error)
+    }
+}
+
+struct JsonWriter<'a, 'b: 'a>(&'a mut fmt::Formatter<'b>);
+
+impl io::Write for JsonWriter<'_, '_> {
+    #[inline]
+    fn write(&mut self, bytes: &[u8]) -> io::Result<usize> {
+        self.write_all(bytes)?;
+        Ok(bytes.len())
+    }
+
+    #[inline]
+    fn write_all(&mut self, bytes: &[u8]) -> io::Result<()> {
+        write(self.0, bytes).map_err(|err| io::Error::new(io::ErrorKind::InvalidData, err))
+    }
+
+    #[inline]
+    fn flush(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+}
+
+fn write(f: &mut fmt::Formatter<'_>, bytes: &[u8]) -> fmt::Result {
+    let mut last = 0;
+    for (index, byte) in bytes.iter().enumerate() {
+        let escaped = match byte {
+            b'&' => Some(br"\u0026"),
+            b'\'' => Some(br"\u0027"),
+            b'<' => Some(br"\u003c"),
+            b'>' => Some(br"\u003e"),
+            _ => None,
+        };
+        if let Some(escaped) = escaped {
+            f.write_str(unsafe { str::from_utf8_unchecked(&bytes[last..index]) })?;
+            f.write_str(unsafe { str::from_utf8_unchecked(escaped) })?;
+            last = index + 1;
+        }
+    }
+    f.write_str(unsafe { str::from_utf8_unchecked(&bytes[last..]) })
 }
 
 #[cfg(test)]
@@ -29,12 +78,16 @@ mod tests {
 
     #[test]
     fn test_json() {
-        assert_eq!(json(true).unwrap(), "true");
-        assert_eq!(json("foo").unwrap(), r#""foo""#);
-        assert_eq!(json(true).unwrap(), "true");
-        assert_eq!(json("foo").unwrap(), r#""foo""#);
+        assert_eq!(json(true).unwrap().to_string(), "true");
+        assert_eq!(json("foo").unwrap().to_string(), r#""foo""#);
+        assert_eq!(json(true).unwrap().to_string(), "true");
+        assert_eq!(json("foo").unwrap().to_string(), r#""foo""#);
         assert_eq!(
-            json(vec!["foo", "bar"]).unwrap(),
+            json("<script>").unwrap().to_string(),
+            r#""\u003cscript\u003e""#
+        );
+        assert_eq!(
+            json(vec!["foo", "bar"]).unwrap().to_string(),
             r#"[
   "foo",
   "bar"
